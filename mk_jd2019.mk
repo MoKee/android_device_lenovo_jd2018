12a2f0c91978ab3161a3032514510615567c95f3
Art_Chen <Chenxy0201@qq.com>
2019-04-02 23:41:07 +0800

    jd2019: Add un-tracked files

---
+++ mk_jd2019.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/violet/device.mk)
+
+# Inherit some common Mokee stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := mk_jd2019
+PRODUCT_DEVICE := jd2019
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Lenovo Z5s
+PRODUCT_MANUFACTURER := Lenovo
+
+BUILD_FINGERPRINT := "Lenovo/jd2019/jd2019:9/PKQ1.180929.001/10.7.303_190315:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="jd2019-user 9 PKQ1.180929.001 10.7.303_190315 release-keys" \
+    PRODUCT_NAME="jd2019" \
+    TARGET_DEVICE="jd2019"
+
+PRODUCT_GMS_CLIENTID_BASE := android-lenovo
