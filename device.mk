7d9b4e7188d904a11d286824a76578efe3fe5813
Art_Chen <chenxy0201@qq.com>
2019-11-02 21:52:03 +0800

    jd2018:Create system copy of ims-ext-common and add to classpath
     * Our ims.apk doesn't load ims-ext-common.jar, and it's moved to /product
       on 10 where PRODUCT_BOOT_JARS doesn't work.
     * Create a system copy of the jar and add it to boot classpath to make
       it loadable from the apk without extra work.

--- device.mk
+++ device.mk
@@ -256,8 +256,11 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
-    ims-ext-common \
+    ims-ext-common_system \
     ims_ext_common.xml
+
+PRODUCT_BOOT_JARS += \
+		ims-ext-common_system
 
 # IPACM
 PRODUCT_PACKAGES += \
